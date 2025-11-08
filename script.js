2e3897d2aff0cbdd6252960c2fdb40d662d26129
bin-ruc <ruchiparigupta@gmail.com>
2025-11-07 22:15:24 -0500

    Refactor modal close functionality
    Removed redundant modal open and close functions.

--- script.js
+++ script.js
@@ -32,20 +32,6 @@ window.onclick = function(event) {
   const modal = document.getElementById("productModal");
   if (event.target === modal) {
     modal.style.display = "none";
-    function openModal(id) {
-  document.getElementById(id).style.display = "block";
-}
-
-function closeModal(id) {
-  document.getElementById(id).style.display = "none";
-}
-
-// close modal when clicking outside
-window.onclick = function(event) {
-  if (event.target.classList.contains('modal')) {
-    event.target.style.display = "none";
-  }
-}
-
   }
 };
+
